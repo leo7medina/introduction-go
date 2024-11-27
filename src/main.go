9709012ccb3a293323eef2915997b53c3b789e37
Leonardo Medina <leo_medina14@hotmail.com>
2024-11-27 00:32:06 -0500

    variables, constantes y zero values

--- src/main.go
+++ src/main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Mundo, go pal mundo...!")
+	//Delcaracion de contstantes
+	const message string = "Hola Mundo, Go pal mundo...!"
+	const pi float64 = 3.14
+	const pi2 = 3.15
+	fmt.Println(message)
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
 }
