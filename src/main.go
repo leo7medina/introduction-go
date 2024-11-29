fd8dd1c6b39eb2427ea36a5695725d7aac8a4877
Leonardo Medina <leo_medina14@hotmail.com>
2024-11-28 23:10:43 -0500

    arrays y slices

--- src/main.go
+++ src/main.go
@@ -15,7 +15,8 @@ func main() {
 	// fnUsoCiclos()
 	// fnCondicionalIf()
 	// fnUsoSwitch()
-	fnUsoKeyWords()
+	// fnUsoKeyWords()
+	fnUsoArraysAndSlices()
 
 }
 
@@ -343,4 +344,31 @@ func fnUsoKeyWords() {
 			break
 		}
 	}
+}
+
+func fnUsoArraysAndSlices() {
+	// Array  (son inmutables)
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+
+	// Slice (son dinamicos)
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 11)
+	fmt.Println(slice)
+
+	// Append
+	newSlice := []int{12, 13, 14}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
